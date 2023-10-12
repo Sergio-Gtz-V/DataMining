c6868d21bf691a90a6205f6050c5ad66adc29b5f
Sergio-Gtz-V <Sergio-Gtz-V@github.com>
2023-10-12 14:22:32 -0600

    Practica 3

--- practica2.py
+++ practica2.py
@@ -7,5 +7,7 @@ df = pd.read_csv('downloaded_file.csv')
 
 df = df.drop(['GAME_STATUS_TEXT','HOME_TEAM_ID', 'VISITOR_TEAM_ID', 'TEAM_ID_home', 'TEAM_ID_away'], axis = 1)
 
+df = df.rename(columns={"GAME_DATE_EST":"GAME_DATE","HT_NICKNAME": "HOME_TEAM", "VT_NICKNAME":"AWAY_TEAM"})
+
 df.to_csv('cleaned_database.csv', index=False)
 
